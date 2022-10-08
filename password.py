feff5db1b23db17d846479bbb5178f104628cf8f
Allen <qs96649006@gmail.com>
2022-10-08 10:11:19 +0800

    chang to while i > 0

--- password.py
+++ password.py
@@ -1,6 +1,6 @@
 password = "a123456" #可以自動化修改密碼?結合讀取txt或json檔修改
 i = 3
-while True:
+while i > 0:
 	account_password = input("請輸入密碼")
 	if account_password == password:
 		print("登入成功")
@@ -8,6 +8,4 @@ while True:
 	else:
 		i -= 1
 		print(f"密碼錯誤,還有{i}次機會,請重新輸入")
-		if i == 0:
-			print("登入失敗,超過登入次數,補發登入密碼")
-			break
+		
